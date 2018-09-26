2e9cea986020fcd2720115d845be50d397eba6a1
Divanshu Rohatgi <divanshu.rohatgi@tothenew.com>
2018-09-26 13:15:31 +0530

    Java Exercise 1 and 2

---
+++ src/com/exercise/second/Question2.java
@@ -0,0 +1,16 @@
+package com.exercise.second;
+
+public class Question2 {
+
+
+    public static void sortString(String inputString) {
+
+        String string = "sjkfahdlkas";
+        System.out.println("Sorted String :" + string);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
