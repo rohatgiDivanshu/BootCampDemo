0f9151e155c8794544ef5007baf6d0bf97376b43
Divanshu Rohatgi <divanshu.rohatgi@tothenew.com>
2018-09-28 09:59:11 +0530

    Collections Exercise

--- src/com/exercise/second/Question13.java
+++ src/com/exercise/second/Question13.java
@@ -1,7 +1,5 @@
 package com.exercise.second;
 
-import java.util.Scanner;
-
 class AgeIsNegativeException extends Exception {
     String errorMessage;
 
@@ -13,20 +11,33 @@ class AgeIsNegativeException extends Exception {
     public String toString() {
         return errorMessage;
     }
+
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+        String s = null;
+    }
 }
 
 
-public class Question13 {
+public class Question13 extends AgeIsNegativeException {
+
+    public Question13(String errorMessage) {
+        super(errorMessage);
+    }
+
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Question13 question13 = new Question13("Age can not be negative");
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter Your Age");
+//
+//        int age = sc.nextInt();
 
-        System.out.println("Enter Your Age");
-
-        int age = sc.nextInt();
-
-        try {
+/*        try {
             if (age < 0) {
                 throw new AgeIsNegativeException("Age can not be negative");
             } else {
@@ -34,7 +45,13 @@ public class Question13 {
             }
         } catch (AgeIsNegativeException ex) {
             System.out.println(ex);
-        }
+            ex.printStackTrace();
+
+            System.out.println();
+        }*/
+
+        System.out.println(question13.errorMessage);
+//        question13.printStackTrace();
     }
 
 }
